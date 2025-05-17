f429f5de9978825ad30379074eea6c5497a340fe
sam-ovalle <samovalle878@gmail.com>
2025-05-17 01:26:02 +0000

    modified heartbeat.py to start a node for 50 seconds and then shut it down

--- mars_rover_systems/mars_rover_systems/heartbeat.py
+++ mars_rover_systems/mars_rover_systems/heartbeat.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 import rclpy
+import time
+from rclpy.node import Node
 
 def main(args=None):
     # initialize the ROS communication
     rclpy.init(args=args)
-    # print a message to the terminal
-    print("Mars rover 1 is alive...")
+
+    # Create a node
+    node = Node('mars_rover_1')
+
+    i = 0
+    max_i = 50
+    while i < max_i:
+        i += 1
+        time_stamp = time.time()
+        # print a message to the terminal
+        print(str(i)+":Mars rover 1 is alive..."+str(time_stamp))
+        # Wait for 1 second
+        time.sleep(1)
+    
     # shutdown the ROS communication
+    node.destroy_node()
     rclpy.shutdown()
 
 def main_shutdown(args=None):
