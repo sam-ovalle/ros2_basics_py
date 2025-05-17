c11afdef76e2be54665666157f1fd7815c5d3d47
sam-ovalle <samovalle878@gmail.com>
2025-05-17 13:21:12 +0000

    demo of a subscriber reading laser scan data

--- mars_rover_tasks/mars_rover_tasks/subscriber_obstacle_detector.py
+++ mars_rover_tasks/mars_rover_tasks/subscriber_obstacle_detector.py
@@ -2,16 +2,39 @@
 
 import rclpy
 from rclpy.node import Node
+# import the LaserScan module from sensor_msgs interface
+from sensor_msgs.msg import LaserScan
+# import Quality of Service library, to set the correct profile and reliability to read sensor data.
+from rclpy.qos import ReliabilityPolicy, QoSProfile
 
 class ObstacleDetectorNode(Node):
     def __init__(self, node_name="obstacle_detector_node"):
         self._node_name = node_name
         super().__init__(self._node_name)
+
+                # create the subscriber object
+        # in this case, the subscriptor will be subscribed on /scan topic with a queue size of 10 messages.
+        # use the LaserScan module for /scan topic
+        # send the received info to the laserscan_callback method.
+        self.subscriber = self.create_subscription(
+            LaserScan,
+            '/laser_scan',
+            self.laserscan_callback,
+            QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE))  # is the most used to read LaserScan data
+
         self.get_logger().info(self._node_name +" Ready...")
+
+    def laserscan_callback(self, msg):
+        # Find the minimum distance in the ranges array
+        min_distance = min(msg.ranges)
+        
+        # Log the minimum distance value
+        self.get_logger().info(f'Minimum distance: {min_distance:.2f} meters')
         
 def main(args=None):
     rclpy.init(args=args)
     node = ObstacleDetectorNode()
+    rclpy.spin(node)
     rclpy.shutdown()
 
 if __name__ == '__main__':
