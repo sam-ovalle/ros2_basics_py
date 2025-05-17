121b582543115f048f1060554b0661c89896d7e0
sam-ovalle <samovalle878@gmail.com>
2025-05-17 13:38:23 +0000

    modified publisher to end with the robot stopping when CTRL+C is pressed

--- mars_rover_tasks/mars_rover_tasks/publish_mars_rover_move.py
+++ mars_rover_tasks/mars_rover_tasks/publish_mars_rover_move.py
@@ -1,53 +1,43 @@
 #!/usr/bin/env python
 
 import rclpy
-# import the ROS2 python libraries
+import signal
 from rclpy.node import Node
-# import the Twist interface from the geometry_msgs package
 from geometry_msgs.msg import Twist
 
 class MoveRoverNode(Node):
 
     def __init__(self):
-        # Here you have the class constructor
-        # call super() in the constructor to initialize the Node object
-        # the parameter you pass is the node name
         super().__init__('move_rover_node')
-        # create the publisher object
-        # in this case, the publisher will publish on /cmd_vel Topic with a queue size of 10 messages.
-        # use the Twist module for /cmd_vel Topic
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
-        # define the timer period for 0.5 seconds
         timer_period = 0.5
-        # create a timer sending two parameters:
-        # - the duration between 2 callbacks (0.5 seconds)
-        # - the timer function (timer_callback)
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
-        # Here you have the callback method
-        # create a Twist message
         msg = Twist()
-        # define the linear x-axis velocity of /cmd_vel Topic parameter to 0.5
         msg.linear.x = 0.5
-        # define the angular z-axis velocity of /cmd_vel Topic parameter to 0.5
         msg.angular.z = 0.5
-        # Publish the message to the Topic
         self.publisher_.publish(msg)
-        # Display the message on the console
         self.get_logger().info('Publishing: "%s"' % msg)
-            
+    
+    def stop_rover(self):
+        stop_msg = Twist()  # All fields default to zero, which represents stopping the rover
+        self.publisher_.publish(stop_msg)
+        self.get_logger().info('Publishing stop message before shutdown')
+
 def main(args=None):
-    # initialize the ROS communication
     rclpy.init(args=args)
-    # declare the node constructor
     simple_publisher = MoveRoverNode()
-    # pause the program execution, waits for a request to kill the node (ctrl+c)
+
+    def signal_handler(sig, frame):
+        simple_publisher.stop_rover()
+        simple_publisher.destroy_node()
+        rclpy.shutdown()
+
+    # Register the signal handler for CTRL+C
+    signal.signal(signal.SIGINT, signal_handler)
+
     rclpy.spin(simple_publisher)
-    # Explicity destroys the node
-    simple_publisher.destroy_node()
-    # shutdown the ROS communication
-    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
