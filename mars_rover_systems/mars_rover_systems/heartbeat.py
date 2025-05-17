a8cf803f4bccb1e94a95d8b62eedd9ee16ec4ad3
sam-ovalle <samovalle878@gmail.com>
2025-05-17 01:35:11 +0000

    coverted heartbeat.py to OOP

--- mars_rover_systems/mars_rover_systems/heartbeat.py
+++ mars_rover_systems/mars_rover_systems/heartbeat.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python
+
 import rclpy
-import time
 from rclpy.node import Node
 
+class HeartbeatNode(Node):
+    def __init__(self, rover_name, timer_period=0.2):
+        # call super() in the constructor to initialize the Node object
+        # the parameter we pass is the node name
+        self._rover_name = rover_name
+        super().__init__(self._rover_name)
+        # create a timer sending two parameters:
+        # - the duration between two callbacks (0.2 seconds)
+        # - the timer function (timer_callback)
+        self.create_timer(timer_period, self.timer_callback)
+        
+    def timer_callback(self):
+        ros_time_stamp = self.get_clock().now()
+        self.get_logger().info(self._rover_name +" is alive..."+str(ros_time_stamp))
+
 def main(args=None):
-    # initialize the ROS communication
+    # initialize the ROS2 communication
     rclpy.init(args=args)
-
-    # Create a node
-    node = Node('mars_rover_1')
-
-    i = 0
-    max_i = 50
-    while i < max_i:
-        i += 1
-        time_stamp = time.time()
-        # print a message to the terminal
-        print(str(i)+":Mars rover 1 is alive..."+str(time_stamp))
-        # Wait for 1 second
-        time.sleep(1)
-    
-    # shutdown the ROS communication
-    node.destroy_node()
-    rclpy.shutdown()
-
-def main_shutdown(args=None):
-    rclpy.init(args=args)
-    print("Shutting down Mars rover 1...")
+    # declare the node constructor
+    node = HeartbeatNode(rover_name="mars_rover_1", timer_period=1.0)
+    # keeps the node alive, waits for a request to kill the node (ctrl+c)
+    rclpy.spin(node)
+    # shutdown the ROS2 communication
     rclpy.shutdown()
 
 if __name__ == '__main__':
-    main() #call the main function
+    main()
